7da9d5556167469ab634ea8922f562a83d74d5c6
Shivansh Mittal <www.shivansh065@gmail.com>
2024-08-16 08:56:24 +0530

    added a ques. prime

---
+++ Functions/functions14_checkprime.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+bool checkprime(int n)
+{
+    int c=0;
+    for(int i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+            if(c>1)
+                break;
+        }
+    }
+    if(c>1)
+        return 0;
+    else
+        return 1;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a = checkprime(n);
+    if(a==1)
+        cout<<"Yes, n is prime";
+    else
+        cout<<"Yes, n is not prime";
+}
